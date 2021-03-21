bc1c069b396af6ea70f3efa8eba32cb719d9d5a7
Max <bazuka5801@mail.ru>
2021-03-21 17:59:11 +0300

    fix(android): logic error

--- android/src/main/java/com/bazumax/capacitor/firebase/auth/FirebaseAuth.kt
+++ android/src/main/java/com/bazumax/capacitor/firebase/auth/FirebaseAuth.kt
@@ -9,7 +9,7 @@ class FirebaseAuth {
 
     fun getClaims(call: PluginCall, success: (MutableSet<String>) -> Unit) {
         val user = Firebase.auth.currentUser
-        if (user != null) {
+        if (user == null) {
             call.reject("User is not authenticated")
             return
         }
